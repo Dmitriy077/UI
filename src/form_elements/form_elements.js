e098bc913c29d6838ef2401db64cb3cfdc57d923
Dmitriy Chapaev <dsergeevic077@gmail.com>
2019-04-18 15:52:07 +0500

    +full form element

--- src/form_elements/form_elements.js
+++ src/form_elements/form_elements.js
@@ -1,3 +1,4 @@
+//form button
 $('.form_elements_button').on('click', function() {
 
 	var name = $('.message_form_name').val();
@@ -19,4 +20,70 @@ $('.form_elements_button').on('click', function() {
 		$('.message_form_thanks_email').css("display", "none");
 	}
 
-	});
+});
+
+//toggles
+$('.toggles_status_off_position_left').on('click', function() {
+
+	$('.toggles_status_off_position_left').css("display", "none");
+	$('.toggles_status_on_position_left').css("display", "inline-block");
+	$('.toggles_status_on_position_right').css("display", "none");
+	$('.toggles_status_off_position_right').css("display", "inline-block");
+
+});
+
+$('.toggles_status_on_position_right').on('click', function() {
+
+	$('.toggles_status_off_position_left').css("display", "none");
+	$('.toggles_status_on_position_left').css("display", "inline-block");
+	$('.toggles_status_on_position_right').css("display", "none");
+	$('.toggles_status_off_position_right').css("display", "inline-block");
+
+});
+
+$('.toggles_status_on_position_left').on('click', function() {
+
+	$('.toggles_status_off_position_left').css("display", "inline-block");
+	$('.toggles_status_on_position_left').css("display", "none");
+	$('.toggles_status_on_position_right').css("display", "inline-block");
+	$('.toggles_status_off_position_right').css("display", "none");
+
+});
+
+$('.toggles_status_off_position_right').on('click', function() {
+
+	$('.toggles_status_off_position_left').css("display", "inline-block");
+	$('.toggles_status_on_position_left').css("display", "none");
+	$('.toggles_status_on_position_right').css("display", "inline-block");
+	$('.toggles_status_off_position_right').css("display", "none");
+
+});
+
+// tick
+$('.tick_status_on_position_left').on('click', function() {
+
+	$('.tick_status_on_position_left').css("display", "none");
+	$('.tick_status_off_position_left').css("display", "inline-block");
+
+});
+
+$('.tick_status_off_position_left').on('click', function() {
+
+	$('.tick_status_on_position_left').css("display", "inline-block");
+	$('.tick_status_off_position_left').css("display", "none");
+
+});
+
+$('.tick_status_on_position_right').on('click', function() {
+
+	$('.tick_status_on_position_right').css("display", "none");
+	$('.tick_status_off_position_right').css("display", "inline-block");
+
+});
+
+$('.tick_status_off_position_right').on('click', function() {
+
+	$('.tick_status_on_position_right').css("display", "inline-block");
+	$('.tick_status_off_position_right').css("display", "none");
+
+});
