52691f71df4b1e7bbf0ec98b6e24688846a5d516
Dmitriy Chapaev <dsergeevic077@gmail.com>
2019-08-07 15:01:36 +0500

    final video

---
+++ src/content/video/video.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+	var myPlayer = $('.video_content').RTOP_VideoPlayer({
+		// fontAwesomeControlIcons: false,
+		showControls: true,
+	    showControlsOnHover: true,
+	    controlsHoverSensitivity: 3000,
+	    showScrubber: true,
+	    showTimer: false,
+	    showPlayPauseBtn: true,
+	    showSoundControl: false,
+	    showFullScreen: true,
+	    keyboardControls: true,
+	    
+
+	});
+
+});
